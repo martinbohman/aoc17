30bb07b34c3506a9ba2ad44254bb0ff4c7b54b0c
Martin Bohman <martins.mailadress@gmail.com>
2017-12-05 14:19:37 +0100

    Add day2

---
+++ day2/main.rb
@@ -0,0 +1,33 @@
+def search_divisible(num, rest_of_line)
+  rest_of_line.each do |num2|
+    higher = [num, num2].max
+    lower = [num, num2].min
+    if higher % lower == 0
+      return higher / lower
+    end
+  end
+  nil
+end
+
+file = File.open('input.txt', 'r')
+checksum1 = 0
+checksum2 = 0
+file.readlines.each do |line|
+  min = Float::INFINITY
+  max = 0
+  line_divisible_found = false
+  numbers = line.split(' ').map(&:to_i)
+  numbers.each_with_index do |num, i|
+    min = num if num < min
+    max = num if num > max
+    rest_of_line = numbers.reject.with_index{ |_n, j| j == i }
+    divisible_ratio = search_divisible(num, rest_of_line)
+    if !line_divisible_found && divisible_ratio
+      line_divisible_found = true
+      checksum2 += divisible_ratio
+    end
+  end
+  checksum1 += (max - min)
+end
+puts checksum1
+puts checksum2
