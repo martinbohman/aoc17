f67079ae089a580e674102cebb4dd1c4c886145e
Martin Bohman <martins.mailadress@gmail.com>
2017-12-05 08:47:24 +0100

    Add day1

---
+++ day1/main.rb
@@ -0,0 +1,13 @@
+file = File.open('input.txt', 'r')
+line = file.readlines.first
+sum1 = 0
+sum2 = 0
+len = line.length
+i = 0
+for i in 0..len
+  num = line[i]
+  sum1 += num.to_i if num == line[(i + 1) % len]
+  sum2 += num.to_i if num == line[(i + len / 2) % len]
+end
+puts sum1
+puts sum2
